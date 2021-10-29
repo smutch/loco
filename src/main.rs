01a02f7b0fbb45737f45501137c786286237ee97
Simon Mutch <smutch@unimelb.edu.au>
2021-10-29 00:11:41 +0000

    Refactor and play with some tricks

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{format_err, Context, Result};
 use colored::Colorize;
+use lazy_static::lazy_static;
 use serde_derive::Deserialize;
 use spinners::{Spinner, Spinners};
 use std::io::Write;
@@ -7,16 +8,6 @@ use std::path::PathBuf;
 use std::process::Command;
 use structopt::StructOpt;
 use xdg::BaseDirectories;
-
-#[derive(StructOpt, Debug)]
-struct Opt {
-    #[structopt(parse(from_os_str))]
-    path: PathBuf,
-    dest: Option<PathBuf>,
-    port: Option<u16>,
-    username: Option<String>,
-    verbose: Option<bool>,
-}
 
 #[derive(Deserialize, Debug)]
 struct LocoConfig {
@@ -40,24 +31,32 @@ impl LocoConfig {
     }
 }
 
+lazy_static! {
+    static ref LOCOCONFIG: LocoConfig = LocoConfig::new()
+        .context("Failed to parse config.")
+        .unwrap();
+    static ref LOCOPORT: String = LOCOCONFIG.port.to_string();
+}
+
+#[derive(StructOpt, Debug)]
+struct Opt {
+    #[structopt(parse(from_os_str))]
+    src: Vec<PathBuf>,
+    #[structopt(parse(from_os_str), short, default_value = LOCOCONFIG.dest.to_str().unwrap())]
+    dest: PathBuf,
+    #[structopt(short, default_value = &LOCOPORT)]
+    port: u16,
+    #[structopt(short, default_value = &LOCOCONFIG.username)]
+    username: String,
+    #[structopt(short)]
+    verbose: bool,
+}
+
 fn main() -> Result<()> {
     let opt = Opt::from_args();
-    std::fs::metadata(&opt.path).context("Failed to resolve source path")?;
-
-    let loco_config = LocoConfig::new().context("Failed to parse config.")?;
-
-    // TODO: Try out some macro magic for this...
-    let dest = opt.dest.unwrap_or(loco_config.dest);
-    let port = opt.port.unwrap_or(loco_config.port);
-    let username = opt.username.unwrap_or(loco_config.username);
-
-    let dest = dest.to_str().unwrap();
-    let spinner = Spinner::new(
-        &Spinners::Dots9,
-        format!("{} {}\n", "Making a local copy of".blue(), dest.blue()),
-    );
 
     // Ensure we have a trailing slash on our destination directory
+    let dest = opt.dest.to_str().unwrap();
     let dest = &format!(
         "localhost:{}{}",
         dest,
@@ -66,40 +65,52 @@ fn main() -> Result<()> {
 
     // If we don't have a trailing slash in our source then we want to use the '--delete' option to
     // remove files from the destination that shouldn't be there.
-    let source = opt.path.to_str().unwrap();
+    Spinner::new(
+        &Spinners::Dots9,
+        format!("{}: ", "Grabbing files...".blue()),
+        );
 
-    let connection = &format!("ssh -p{} -l{}", &port, &username);
-    let mut args = vec!["-r", "-n", "-e", connection];
+    opt.src.iter().try_for_each(|src| {
+        std::fs::metadata(&src).context(format!(
+                "{} {}",
+                "Failed to resolve".red(),
+                src.to_str().unwrap()
+                ))?;
 
-    if !source.ends_with('/') {
-        args.push("--delete");
-    }
+        let source = src.to_str().unwrap();
 
-    args.push(source);
-    args.push(dest);
+        let connection = &format!("ssh -p{} -l{}", &opt.port, &opt.username);
+        let mut args = vec!["-r", "-n", "-e", connection];
 
-    let command = Command::new("rsync")
-        .args(&args)
-        .output()
-        .context("Failed to launch rsync command")?;
+        if !source.ends_with('/') {
+            args.push("--delete");
+        }
 
-    spinner.stop();
+        args.push(source);
+        args.push(dest);
 
-    match command.status.success() {
-        true => {
-            if opt.verbose.unwrap_or(false) {
-                println!();
-                std::io::stdout().write_all(command.stdout.as_slice())?;
+        let command = Command::new("rsync")
+            .args(&args)
+            .output()
+            .context("Failed to launch rsync command")?;
+
+        match command.status.success() {
+            true => {
+                if opt.verbose {
+                    println!();
+                    std::io::stdout().write_all(command.stdout.as_slice())?;
+                }
+                Ok(())
             }
-            Ok(())
+            false => {
+                std::io::stdout().write_all(command.stdout.as_slice())?;
+                let errors = format!("\n{}\n", String::from_utf8(command.stderr).unwrap());
+                std::io::stderr().write_all(errors.as_bytes())?;
+                Err(format_err!("{}", "Rsync command failed!".red()))
+            }
         }
-        false => {
-            std::io::stdout().write_all(command.stdout.as_slice())?;
-            println!();
-            let errors = format!("{}", String::from_utf8(command.stderr).unwrap().red());
-            std::io::stderr().write_all(errors.as_bytes())?;
-            println!();
-            Err(format_err!("Rsync command failed!"))
-        }
-    }
+    }).map(|res| {
+        println!("{}", "...transfer complete".green().bold());
+        res
+    })
 }
